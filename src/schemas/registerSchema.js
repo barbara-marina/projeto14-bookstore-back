0fd2b2905cc959f01959a54cc0d303acf5f924c9
Matheus Chonge <matheus.chong@gmail.com>
2022-05-15 16:14:45 -0300

    Feat Register account

--- src/schemas/registerSchema.js
+++ src/schemas/registerSchema.js
@@ -5,7 +5,7 @@ const registerSchema = joi.object({
     email: joi.string().email().required(),
     nome: joi.string().required(),
     senha: joi.string().alphanum().min(8).regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).required()
-    
+
 });
 
 export default registerSchema;
