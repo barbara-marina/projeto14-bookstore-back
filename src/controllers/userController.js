a1919b11309e0f27b084398c56070dfbb839b0e4
Barbarina <barbaramarinaufsm@gmail.com>
2022-05-15 16:06:43 -0300

    feat: add book to shopping cart endpoint

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import db from "./../db.js";
 
 export async function getCategories(_req, res) {
@@ -36,5 +37,37 @@ export async function logout (req, res) {
         res.sendStatus(200);
     } catch (e) {
         return res.sendStatus(500);
+    }
+}
+
+export async function addToShoppingCart(req, res) {
+    const { user } = res.locals;
+    const { bookId } = req.body;
+    
+    try {
+        const book = await db.collection("Books").findOne({_id: new ObjectId(bookId)});
+        if (!book) return res.sendStatus(401);
+
+        await db.collection("UserShoppingCart").updateOne(
+            {cpf: user.cpf},
+            {$push: {shoppingCart: book}}
+        );
+
+        res.status(201).send("Livro adicionado ao seu carrinho.");
+    } catch (e) {
+        return res.sendStatus(500);
+    }
+}
+
+export async function getShoppingCart(_req, res) {
+    const { user } = res.locals;
+
+    try {
+        const shoppingCart = await db.collection("UserShoppingCart").findOne({cpf: user.cpf});
+        if (!shoppingCart) return res.sendStatus(401);
+
+        res.send(shoppingCart);
+    } catch (e) {
+        return res,sendStatus(500);
     }
 }
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {getCategories, getBooksData, logout} from "../controllers/userController.js";
+import {getCategories, getBooksData, logout, addToShoppingCart, getShoppingCart} from "../controllers/userController.js";
 import validateToken from "../middlewares/tokenValidationMiddleware.js";
 
 
@@ -8,5 +8,7 @@ const userRouter = Router();
 userRouter.get("/categories", getCategories);
 userRouter.get("/books", getBooksData);
 userRouter.delete("/logout", validateToken, logout);
+userRouter.post("/user/shoppingCart", validateToken, addToShoppingCart);
+userRouter.get("/user/shoppingCart", validateToken, getShoppingCart);
 
 export default userRouter;
